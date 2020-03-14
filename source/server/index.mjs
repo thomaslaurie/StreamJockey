1e58a9c3734cf1d044041734928d91cdf8728e6a
thomaslaurie <thomas.laurie@outlook.com>
2020-03-14 14:41:47 -0700

    Working full build with all current changes.

---
+++ source/server/index.mjs
@@ -0,0 +1,234 @@
+// ███╗   ██╗ ██████╗ ████████╗███████╗███████╗
+// ████╗  ██║██╔═══██╗╚══██╔══╝██╔════╝██╔════╝
+// ██╔██╗ ██║██║   ██║   ██║   █████╗  ███████╗
+// ██║╚██╗██║██║   ██║   ██║   ██╔══╝  ╚════██║
+// ██║ ╚████║╚██████╔╝   ██║   ███████╗███████║
+// ╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝
+
+/*
+	//L process.env: https://www.twilio.com/blog/2017/08/working-with-environment-variables-in-node-js.html
+	//L dotenv:  https://www.npmjs.com/package/dotenv
+*/
+
+
+//  ████████╗ ██████╗ ██████╗  ██████╗ 
+//  ╚══██╔══╝██╔═══██╗██╔══██╗██╔═══██╗
+//     ██║   ██║   ██║██║  ██║██║   ██║
+//     ██║   ██║   ██║██║  ██║██║   ██║
+//     ██║   ╚██████╔╝██████╔╝╚██████╔╝
+//     ╚═╝    ╚═════╝ ╚═════╝  ╚═════╝ 
+
+/* 
+	Put api keys into .env after creating methods to access them
+	Some best practices: https://www.codementor.io/mattgoldspink/nodejs-best-practices-du1086jja
+	Middleware best practices https://github.com/koajs/koa/blob/master/docs/guide.md
+
+	errors thrown in some places (like routes) still aren't caught
+*/
+
+
+//  ██████╗ ███████╗██████╗ ███████╗███╗   ██╗██████╗ ███████╗███╗   ██╗ ██████╗██╗███████╗███████╗
+//  ██╔══██╗██╔════╝██╔══██╗██╔════╝████╗  ██║██╔══██╗██╔════╝████╗  ██║██╔════╝██║██╔════╝██╔════╝
+//  ██║  ██║█████╗  ██████╔╝█████╗  ██╔██╗ ██║██║  ██║█████╗  ██╔██╗ ██║██║     ██║█████╗  ███████╗
+//  ██║  ██║██╔══╝  ██╔═══╝ ██╔══╝  ██║╚██╗██║██║  ██║██╔══╝  ██║╚██╗██║██║     ██║██╔══╝  ╚════██║
+//  ██████╔╝███████╗██║     ███████╗██║ ╚████║██████╔╝███████╗██║ ╚████║╚██████╗██║███████╗███████║
+//  ╚═════╝ ╚══════╝╚═╝     ╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═══╝ ╚═════╝╚═╝╚══════╝╚══════╝
+
+// top
+import '../config/config.mjs';
+
+// builtin
+
+// external
+// import parser from 'minimist';
+// import webpack from 'webpack';
+import Koa from 'koa'; //L https://github.com/koajs
+// import koaWebpack from 'koa-webpack';
+import bodyParser from 'koa-bodyparser'; //L https://github.com/koajs/bodyparser
+import session from 'koa-session'; //L https://github.com/koajs/session
+
+//L https://github.com/socketio/socket.io#in-conjunction-with-koa
+import SocketIO from 'socket.io'; //L socket io: https://socket.io/docs/emit-cheatsheet
+import http from 'http'; //TODO consider changing to the https module?
+
+
+// internal
+// import { client } from '../config/webpack.config2.mjs'
+import sj from './global-server.mjs';
+import router from './routes.mjs';
+
+
+//  ██╗███╗   ██╗██╗████████╗
+//  ██║████╗  ██║██║╚══██╔══╝
+//  ██║██╔██╗ ██║██║   ██║   
+//  ██║██║╚██╗██║██║   ██║   
+//  ██║██║ ╚████║██║   ██║   
+//  ╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝   
+
+// OPTIONS
+// const serverOptions = parser(process.argv.slice(2), {
+// 	string: [
+// 		'client-build-method',
+// 		'client-mode',
+// 	],
+// 	default: {
+// 		/* Options
+// 			'compile'  - builds the client code once
+// 			'watch'    - watches the client code
+// 			'refresh'  - use webpack-dev-middleware
+// 			'hot'      - use webpack HMR
+// 			'external' - assume code is already built/watched by another process
+// 		*/
+// 		'client-build-method': 'external',
+// 		/* Options
+// 			'development' - passed to webpack
+// 			'production'  - passed to webpack
+// 		*/
+// 		'client-mode': 'development',
+// 	},
+// });
+
+// const compiler = webpack(client({}, {
+// 	mode: serverOptions['client-mode'],
+// }));
+// compiler.run((error, stats) => {
+// 	if (error) {
+// 		console.error(error);
+// 		return;
+// 	}
+
+// 	console.log(stats.toString({
+// 		colors: true,
+// 	}));
+// });
+
+// (async function () {
+
+
+
+// const koaWebpackMiddleware = await koaWebpack({
+// 	// compiler: compiler,
+// 	config: client({}, {
+// 		mode: serverOptions['client-mode'],
+// 	}),
+// 	devMiddleware: {
+// 		methods: ['HEAD', 'GET', 'POST', 'PATCH', 'DELETE'],
+// 	},
+// 	hotClient: false,
+// });
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+// KOA
+const app = new Koa();
+app.keys = [process.env.APP_KEY || 'imJustSomeKey'];
+const sessionConfig = {
+	//TODO random keys: //L https://randomkeygen.com/
+
+	//C (string)(default is koa:sess) cookie key
+	key: 'koa:sess', 
+	//C (number || 'session')(default is 1 days) maxAge in ms, 'session' will result in a cookie that expires when session/browser is closed, Warning: If a session cookie is stolen, this cookie will never expire
+	maxAge: 86400000, 
+	//C (boolean)(default true) can overwrite or not
+	overwrite: true,
+	//C (boolean)(default true) httpOnly or not , httpOnly cookies tell the browser not to expose them to client-side script (so that they can only be opened by the server)
+	httpOnly: true,
+	//C (boolean)(default true) signed or not , signed cookies verify that the data is unchanged on the client side
+	signed: true,
+	//C (boolean)(default false) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. , I think this means that the session is reset after every request? (that is that the maxAge is basically since the last time the user made a request)
+	rolling: true,
+	//C (boolean)(default is false) renew session when session is nearly expired, so we can always keep user logged in, //? does this mean never expiring sessions?
+	renew: false, 
+};
+
+//L https://github.com/socketio/socket.io#in-conjunction-with-koa
+const server = http.createServer(app.callback());
+
+// SOCKET IO
+const socketIO = new SocketIO(server);
+sj.liveData.socket = socketIO.of('/live-data');
+
+
+//  ███╗   ███╗██╗██████╗ ██████╗ ██╗     ███████╗██╗    ██╗ █████╗ ██████╗ ███████╗
+//  ████╗ ████║██║██╔══██╗██╔══██╗██║     ██╔════╝██║    ██║██╔══██╗██╔══██╗██╔════╝
+//  ██╔████╔██║██║██║  ██║██║  ██║██║     █████╗  ██║ █╗ ██║███████║██████╔╝█████╗  
+//  ██║╚██╔╝██║██║██║  ██║██║  ██║██║     ██╔══╝  ██║███╗██║██╔══██║██╔══██╗██╔══╝  
+//  ██║ ╚═╝ ██║██║██████╔╝██████╔╝███████╗███████╗╚███╔███╔╝██║  ██║██║  ██║███████╗
+//  ╚═╝     ╚═╝╚═╝╚═════╝ ╚═════╝ ╚══════╝╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+
+// app.use(koaWebpackMiddleware);
+
+/*
+	// response timer
+	app.use(async (ctx, next) => {
+		const start = Date.now();
+		await next();
+		const ms = Date.now() - start;
+		ctx.response.set('response-time', `${ms}ms`);
+	});
+
+	// request logger
+	app.use(async (ctx, next) => {
+		console.log(`${ctx.request.method} ${ctx.request.path}`);
+		await next();
+	});
+*/
+
+// BODY PARSER
+app.use(bodyParser());
+
+// SESSION
+app.use(session(sessionConfig, app));
+
+/*
+	// view counter
+	app.use(async (ctx, next) => {
+		// ignore favicon
+		// TODO this doesn't work as it's processing two 'requests' per view - the page and it's resources
+		if (ctx.request.path !== '/favicon.ico') {
+			let n = ctx.session.views || 0;
+			ctx.session.views = ++n;
+		} 
+		//console.log(ctx.session.views + ' views');
+		await next();
+	});
+*/
+
+// ROUTES
+app.use(router.routes());
+
+//L https://github.com/alexmingoia/koa-router#routerallowedmethodsoptions--function
+app.use(router.allowedMethods()); 
+
+// LIVE DATA
+sj.liveData.start({
+	app,
+	socket: socketIO.of('/live-data'),
+});
+
+
+//  ██╗     ██╗███████╗████████╗███████╗███╗   ██╗
+//  ██║     ██║██╔════╝╚══██╔══╝██╔════╝████╗  ██║
+//  ██║     ██║███████╗   ██║   █████╗  ██╔██╗ ██║
+//  ██║     ██║╚════██║   ██║   ██╔══╝  ██║╚██╗██║
+//  ███████╗██║███████║   ██║   ███████╗██║ ╚████║
+//  ╚══════╝╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═══╝
+
+// START SERVER
+//G Connect at http://localhost:<PORT>
+server.listen(PORT, () => {
+	console.log(`█████████████████████████████`);
+	console.log(`SERVER LISTENING ON PORT ${PORT}`);
+});
+
+//L unhandled errors: https://stackoverflow.com/questions/43834559/how-to-find-which-promises-are-unhandled-in-node-js-unhandledpromiserejectionwar
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at:', p, '\n Reason:', reason);
+    //TODO handle
+});
+
+
+// })();
