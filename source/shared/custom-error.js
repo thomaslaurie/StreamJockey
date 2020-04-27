70053b5ea402d9b34344ad911db32228cddc7788
thomaslaurie <thomas.laurie@outlook.com>
2020-04-26 20:52:50 -0700

    Updated custom error to use new validated variable

--- source/shared/custom-error.js
+++ source/shared/custom-error.js
@@ -13,38 +13,19 @@ export default dynamicClass.create('CustomError', {
 		type = 'Bad Request', // Human-readable name for code.
 		reason = '',          // Human readable message. //! Flipped with 'message' from old sj.Error.
 	} = {}) {
-		let _code;
-		let _type;
-		let _reason;
-
-		define.accessor(this, {
-			get code() {
-				return _code;
+		define.validatedVariable(this, {
+			code: {
+				value: code,
+				validator: rules.integer.validate,
 			},
-			set code(value) {
-				rules.integer.validate(value);
-				_code = value;
+			type: {
+				value: type,
+				validator: rules.string.validate,
 			},
-			get type() {
-				return _type;
+			reason: {
+				value: reason,
+				validator: rules.string.validate,
 			},
-			set type(value) {
-				rules.string.validate(value);
-				_type = value;
-			},
-			get reason() {
-				return _reason;
-			},
-			set reason(value) {
-				rules.string.validate(value);
-				_reason = value;
-			},
-		});
-
-		Object.assign(this, {
-			code,
-			type,
-			reason,
 		});
 	},
 });
